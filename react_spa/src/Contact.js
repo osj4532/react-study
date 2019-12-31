d45f20e954f29b30364d212605f3fda0255168f4
Seoungju Oh <osj4532@daum.net>
2019-12-31 17:44:29 +0900

    make my single page app using react-router-dom

---
+++ react_spa/src/Contact.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Contact extends Component{
+    render(){
+        return (
+            <div>
+                <h2>GOT QUESTIONS?</h2>
+                <p>The easiest thing to do is post on our <a href="https://forum.kirupa.com">formums</a></p>
+            </div>
+        );
+    }
+}
+
+export default Contact;
---
+++ react_spa/src/Home.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+
+class Home extends Component{
+    render(){
+        return (
+            <div>
+                <h2>Hello</h2>
+                <p>
+                    Nulla Lorem mollit cupidatat consequat reprehenderit. Ea eu in incididunt aute. Aliquip aute adipisicing et duis in eu commodo sunt esse magna culpa eu proident.
+                    In mollit proident eu veniam sit magna proident excepteur et ex. Id sit nostrud cillum dolor consectetur laboris ullamco tempor fugiat cupidatat enim ullamco fugiat elit. Dolor eu voluptate veniam adipisicing tempor id proident sint nostrud laboris pariatur aute. Sit tempor incididunt cupidatat ex amet do eiusmod ex et magna. Pariatur eu irure eiusmod ipsum ex duis consectetur qui eu dolore anim ea deserunt. Aliquip sint esse eu aliqua anim laboris consectetur consectetur consectetur.
+                    Culpa consequat pariatur elit labore eiusmod esse veniam cupidatat. Velit enim et ut voluptate in minim. Nulla nulla reprehenderit laboris fugiat consectetur irure proident cupidatat qui nisi ex mollit. Aliquip dolore magna ea irure et aute et consequat velit dolore enim.
+                    Sint qui ad cupidatat elit esse commodo cillum ullamco excepteur nulla consequat quis ea. Consequat enim nisi dolor dolor reprehenderit labore minim eu ullamco exercitation adipisicing incididunt ipsum. Nostrud in voluptate aute reprehenderit non commodo ad enim. Adipisicing consequat mollit ea fugiat velit. Duis officia voluptate dolor culpa. Esse amet est veniam consequat ullamco deserunt cillum qui enim. Sint nulla eu fugiat et ea tempor nisi incididunt.
+                    Duis ea do elit Lorem laboris nulla id laboris consectetur proident ullamco excepteur nostrud anim. Ipsum aliqua ad amet Lorem in Lorem laboris magna adipisicing sunt aliquip velit. Dolore amet duis adipisicing tempor deserunt excepteur officia minim aliqua officia nisi.
+                    Duis ad et labore commodo do irure elit ex voluptate deserunt sint veniam. Cillum tempor aliquip enim ipsum voluptate ex ex Lorem mollit in deserunt id. Incididunt culpa in dolor mollit. Tempor elit non deserunt aliquip laboris consectetur pariatur laboris ea elit deserunt reprehenderit. Ea exercitation voluptate eu nostrud duis excepteur irure quis voluptate sint eu. Sint adipisicing id mollit aute elit. Velit culpa quis veniam est occaecat est commodo sunt.
+                </p>
+
+                <p>Duis a turpis sed lacus dapibus elementum sed eu lectus.</p>
+            </div>
+        );
+    }
+}
+
+export default Home;
